99f6e3547de3caac2ce59ef4b8a6291ce428ef31
auauauau <auauauau@gmail.com>
2025-05-21 22:26:40 -0300

    Inicio portfolio

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<script setup>
+import Navbar from "./components/NavbarComponent.vue";
+</script>
+<template>
+  <Navbar />
+</template>
---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav class="w-full bg-teal-900 text-white flex justify-between p-4">
+    <span class="">Porfolio</span>
+    <ul class="flex gap-10">
+      <li>
+        <router-link to="/home" class="hover:underline">Home</router-link>
+      </li>
+      <li>
+        <router-link to="/about" class="hover:underline">Sobre</router-link>
+      </li>
+      <li>
+        <router-link to="/projects" class="hover:underline"
+          >Projetos</router-link
+        >
+      </li>
+      <li>
+        <router-link to="/contacts" class="hover:underline"
+          >Contato</router-link
+        >
+      </li>
+    </ul>
+  </nav>
+</template>
