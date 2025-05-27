45c640fd2308f4a1e2bbbc3bda96b1ca9eb58dd1
Igor Alves <igor.h.alves.ads@gmail.com>
2025-05-27 00:34:10 -0300

    Adicionado whatsapp ao footer e tela about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,3 +1,25 @@
 <template>
-    <h1>This is an about page</h1>
+<main class="min-h-screen text-white mt-30 ml-50 w-300">
+    <div class="grid grid-cols-2 gap-10">
+        <div class="about">
+            <p class="text-3xl">Olá! Sou o Igor</p>
+            <p class="text-3xl"> Tenho 26 anos e atuo como Desenvolvedor.</p>
+
+            <div class="text-gray-400 text-xl mt-6">
+                <ul class="space-y-6">
+                    <li>
+                        Venho me aprofundando em desenvolvimento web, construção de APIs, bancos de dados e boas práticas de programação. Estou sempre em busca de evoluir tecnicamente, contribuindo com soluções eficientes e bem estruturadas.
+                    </li>
+                    <li>Antes disso, atuei por 4 anos como Analista de Dados, onde desenvolvi habilidades sólidas em Power BI, SQL, Excel, Python e R. Nesse período, fui responsável por transformar dados em insights valiosos que impactaram diretamente na tomada de decisões e nos resultados das empresas em que passei.</li>
+                    <li>Essa transição representa mais do que uma mudança de carreira, é a continuação do meu desejo de resolver problemas de forma criativa e de impactar positivamente o maior número de pessoas possível por meio da tecnologia.</li>
+                </ul>
+
+            </div>
+        </div>
+
+        <div class="image ml-50">
+            <img src="../images/Igor.jpg" alt="">
+        </div>
+    </div>
+</main>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="min-h-screen text-white mt-30 ml-50">
-    <h1 class="text-teal-400 text-xl">Hello World!</h1>
+    <div class="text-teal-400 text-xl">Hello World!</div>
     <div class="mt-5 text-7xl"> Igor Alves - Desenvolvedor Fullstack.</div>
     <div class="mt-5 text-7xl text-gray-400"> Desenvolvendo soluções web</div>
     <div class="w-96 mt-5 text-xl text-gray-400">Atualmente cursando o terceiro semestre de Análise e Desenvlvimento de sistemas, focado em soluções web, analiso e busco a melhor maneira de usar a tecnologia a favor do seu negócio.</div>
